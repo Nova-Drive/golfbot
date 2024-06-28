e7f2fa63487a7a6b42cdac1e5152cb3500f5b236
Cameron <62269723+Nova-Drive@users.noreply.github.com>
2024-06-28 13:41:04 -0600

    Update GolfBot.py

--- GolfBot.py
+++ GolfBot.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-OPEN_TIME = '07:00:00.100000'
+OPEN_TIME = '07:00:00.200000'
 ANTI_TIMEOUT = '06:58:00'
 
 username = ""
