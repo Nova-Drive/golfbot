f5d34b2b6f3476187d75788b9d739471b46aedc3
Cameron <62269723+Nova-Drive@users.noreply.github.com>
2024-09-11 08:47:31 -0600

    Update GolfBot.py

--- GolfBot.py
+++ GolfBot.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-OPEN_TIME = '07:00:00.370000'
+OPEN_TIME = '07:00:00.350000'
 
 # testing
 # OPEN_TIME = '19:15:00.000000'
